1fe61f6e81beb5b3ca48960a9f75f92fa798c605
HungryCodie <106869558+Hungrycoddie@users.noreply.github.com>
2022-07-05 05:01:47 +0530

    Update yt with max resl  in path.py

--- yt with max resl  in path.py
+++ yt with max resl  in path.py
@@ -1,4 +1,8 @@
+#import library 
 import pytube
+#input your url 
 url = input("Enter your Url: ")
-path ="/Users/hungrycodie/Desktop"
+#locate your path in which you want to download 
+path ="Your path here"
+#call the functions
 pytube.YouTube(url).streams.get_highest_resolution().download(path)
